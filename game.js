6960a9011cf916d2156055e83252d0a815479038
RaphaëlBS-WCS <raphaelbilletservoin@gmail.com>
2022-02-14 16:35:44 +0100

    snake base done

---
+++ game.js
@@ -0,0 +1,43 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from "./snake.js";
+import { update as updateFood, draw as drawFood } from './food.js';
+import {outsideGrid} from "./grid.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
+
+function main(currentTime) {
+    if (gameOver) {
+        if(confirm('You loose. Press OK to restart.')) {
+            window.location.reload();
+        }
+        return;
+    }
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRenderTime = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRenderTime < 1 / SNAKE_SPEED) return;
+
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+
+}
+
+window.requestAnimationFrame(main);
+
+export function update() {
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+export function draw() {
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
